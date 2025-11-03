ca6537a8e4e0802e99e2b292fb63d52be44e4f19
theinstantago <douradotiago44@gmail.com>
2025-11-03 12:47:33 -0300

    docs: adiciona comentarios na funcao de anagrama

--- solucoes.py
+++ solucoes.py
@@ -1,12 +1,14 @@
 from collections import Counter
 
 def sao_anagramas(string1, string2):
+    # padronizacao das strings
     string1 = "".join(filter(str.isalnum, string1)).lower()
     string2 = "".join(filter(str.isalnum, string2)).lower()
 
     if len(string1) != len(string2):
         return False
-
+    
+    # verifica se a quantidade de cada caractere unico (key) das strings eh a mesma
     return Counter(string1) == Counter(string2)
 
 def cifra_de_cesar(texto, deslocamento):
